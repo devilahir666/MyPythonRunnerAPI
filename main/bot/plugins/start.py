5f98a67f5a2d37c507b6a78a7930a3c72445cfa1
devilahir666 <bharatchavda00666@gmail.com>
2025-10-19 10:22:00 +0530

    Update start.py

--- main/bot/plugins/start.py
+++ main/bot/plugins/start.py
@@ -1,38 +1,25 @@
 # This file is a part of TG-Direct-Link-Generator
 
+
 from main.bot import StreamBot
 from main.vars import Var
-from pyrogram import filters
-from main.utils.Translation import Language, BUTTON
+from pyrogram import filters, Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(~filters.user(Var.BANNED_USERS) & filters.command('start') & ~filters.edited)
-async def start(b, m):
-    # lang = getattr(Language, m.from_user.language_code)
-    lang = getattr(Language, "en")
-    await m.reply_text(
-        text=lang.START_TEXT.format(m.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=BUTTON.START_BUTTONS
+
+START_TEXT = """**Hello {}**,
+I'm a Telegram Files to Link Generator Bot.
+"""
+
+
+@StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
+async def start_handler(c: Client, m: Message):
+    if m.from_user.id not in Var.BANNED_USERS:
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Support Group 🧑‍💻", url="https://t.me/Devilahir_Bots")]])
+        await m.reply_text(
+            text=START_TEXT.format(m.from_user.first_name),
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
         )
 
-
-@StreamBot.on_message(~filters.user(Var.BANNED_USERS) & filters.command(["about"]) & ~filters.edited)
-async def start(bot, update):
-    # lang = getattr(Language, update.from_user.language_code)
-    lang = getattr(Language, "en")
-    await update.reply_text(
-        text=lang.ABOUT_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=BUTTON.ABOUT_BUTTONS
-    )
-
-
-@StreamBot.on_message((filters.command('help')) & ~filters.edited & ~filters.user(Var.BANNED_USERS))
-async def help_handler(bot, message):
-    # lang = getattr(Language, message.from_user.language_code)
-    lang = getattr(Language, "en")
-    await message.reply_text(
-        text=lang.HELP_TEXT.format(Var.UPDATES_CHANNEL),        
-        disable_web_page_preview=True,
-        reply_markup=BUTTON.HELP_BUTTONS
-    )
+# Yahan koi changes nahi kiye gaye, lekin aapke code mein woh upar waali line galat thi.
