20d712c779c199f9f7cf5c2eb8ce8837acb8fb4a
devilahir666 <bharatchavda00666@gmail.com>
2025-10-20 21:26:01 +0530

    Update vars.py

--- main/vars.py
+++ main/vars.py
@@ -1,5 +1,3 @@
-# This file is a part of TG-Direct-Link-Generator
-
 from os import environ
 from dotenv import load_dotenv
 
@@ -7,38 +5,31 @@ load_dotenv()
 
 
 class Var(object):
-    MULTI_CLIENT = False
+    MULTI_CLIENT = True if environ.get("MULTI_CLIENT", "False").lower() == "true" else False
     API_ID = int(environ.get("API_ID"))
     API_HASH = str(environ.get("API_HASH"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
-    SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minute
-    WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
-    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    MULTI_TOKENS = [
+        str(environ.get(f"MULTI_TOKEN{i}")) for i in range(1, 6) if environ.get(f"MULTI_TOKEN{i}")
+    ]
+    SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
+    WORKERS = int(environ.get("WORKERS", "6"))
+    BIN_CHANNEL = int(environ.get("BIN_CHANNEL", None))
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    HAS_SSL = environ.get("HAS_SSL", False)
-    HAS_SSL = True if str(HAS_SSL).lower() == "true" else False
-    NO_PORT = environ.get("NO_PORT", False)
-    NO_PORT = True if str(NO_PORT).lower() == "true" else False
-    if "DYNO" in environ:
-        ON_HEROKU = True
-        APP_NAME = str(environ.get("APP_NAME"))
-    else:
-        ON_HEROKU = False
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
+    HAS_SSL = str(environ.get("HAS_SSL", "False")).lower() == "true"
+    NO_PORT = str(environ.get("NO_PORT", "False")).lower() == "true"
+
+    ON_HEROKU = "DYNO" in environ
+    APP_NAME = str(environ.get("APP_NAME")) if ON_HEROKU else None
+
     FQDN = (
         str(environ.get("FQDN", BIND_ADDRESS))
         if not ON_HEROKU or environ.get("FQDN")
         else APP_NAME + ".herokuapp.com"
     )
-    if ON_HEROKU:
-        URL = f"https://{FQDN}/"
-    else:
-        URL = "http{}://{}{}/".format(
-            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
-        )
+    URL = f"https://{FQDN}/" if HAS_SSL or ON_HEROKU else f"http://{FQDN}:{PORT}/"
 
     UPDATES_CHANNEL = "TechZBots"
     OWNER_ID = int(environ.get('OWNER_ID', '777000'))
