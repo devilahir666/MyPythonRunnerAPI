9b2ecffa19dfe283f4c6c8665b64a67b15e03864
devilahir666 <bharatchavda00666@gmail.com>
2025-10-20 21:44:45 +0530

    Update StreamBot.py

--- main/StreamBot.py
+++ main/StreamBot.py
@@ -1,6 +1,9 @@
+# This file is a part of TG-Direct-Link-Generator
+
 from pyrogram import Client
 from main.vars import Var
 
+# Single bot instance
 StreamBot = Client(
     "stream_bot",
     api_id=Var.API_ID,
